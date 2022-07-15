f7a97311a5459ff270eb7f4b98c2ba67d1791089
davidkiama <davidkiama98@gmail.com>
2022-07-15 11:12:29 +0300

    Adding properties to the module.exports object

--- modules.js
+++ modules.js
@@ -14,3 +14,10 @@ console.log(require("module").wrapper);
 const C = require("./test-module-1");
 const calc1 = new C();
 console.log(calc1.add(2, 4)); // 6
+
+//exports
+// const calc2 = require("./test-module-2");
+// console.log(calc2.multiply(2, 4)); // 8
+
+const { add, multiply, divide } = require("./test-module-2");
+console.log(divide(10, 2)); // 5
