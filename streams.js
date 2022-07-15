f097073c037e73a7adbff447598005bfaafc3f98
davidkiama <davidkiama98@gmail.com>
2022-07-15 09:58:03 +0300

    Receiving and sending data chunk by chunk

--- streams.js
+++ streams.js
@@ -6,12 +6,31 @@ server.on("request", (req, res) => {
   //Read file into a variable and send it back to the client
   /* This solution will force Node to read the file in its 
      entrirety, save it to memory then send it back
-  */
 
   fs.readFile("./test-file.txt", (err, data) => {
     if (err) return console.log(err);
 
     res.end(data);
+  });
+
+ */
+  // Solution 2
+  /*
+  Here we create a stream that recieves data chunk by chunk.
+  Once we recieve a chunk we immediately send it to the client 
+  and wait for the stream to recieve another chunk.
+  Once we have no more chunks we end the response indicating that 
+  no more data will be sent
+   */
+
+  const readable = fs.createReadStream("./test-file.txt");
+
+  readable.on("data", (chunk) => {
+    res.write(chunk);
+  });
+
+  readable.on("end", () => {
+    res.end();
   });
 });
 
