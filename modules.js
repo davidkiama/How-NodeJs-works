c6b325b488046f17fe7a4ada198fd1e32b48c460
davidkiama <davidkiama98@gmail.com>
2022-07-15 11:03:27 +0300

    module.exports illustration

--- modules.js
+++ modules.js
@@ -1,6 +1,16 @@
-console.log(arguments);
 /*  
+console.log(arguments);
+
 This returns an array of all the argumemnts proving that we are actually
 inside a function (wrapper function)
 
+console.log(require("module").wrapper);
+//Output
+'(function (exports, require, module, __filename, __dirname) { ',
+'\n});'
 */
+
+//module.exports
+const C = require("./test-module-1");
+const calc1 = new C();
+console.log(calc1.add(2, 4)); // 6
